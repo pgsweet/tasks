e63e986dbf47ed140b75d19a37b2e79bb057914e
Patrick Sweet <pgsweet@udel.edu>
2024-02-29 22:00:34 -0500

    TwoDice completed

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,36 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(1);
+    const [rightDie, setRightDie] = useState<number>(6);
+
+    function rollLeft(): void {
+        setLeftDie(d6());
+    }
+
+    function rollRight(): void {
+        setRightDie(d6());
+    }
+
+    return (
+        <div>
+            <div>
+                <Button onClick={rollLeft}>Roll Left</Button>
+                <Button onClick={rollRight}>Roll Right</Button>
+            </div>
+            <div>
+                Left: <span data-testid="left-die">{leftDie}</span>
+                Right: <span data-testid="right-die">{rightDie}</span>
+            </div>
+            <div>
+                {leftDie === 1 && rightDie === 1 ? (
+                    <span>You Lose</span>
+                ) : rightDie === leftDie ? (
+                    <span>You Win</span>
+                ) : (
+                    <span></span>
+                )}
+            </div>
+        </div>
+    );
 }
