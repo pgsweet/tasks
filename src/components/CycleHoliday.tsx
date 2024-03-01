8466f099156e43e9a659a0e7bd82293815d704e1
Patrick Sweet <pgsweet@udel.edu>
2024-03-01 12:44:37 -0500

    CycleHoliday Completed

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,49 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+// 🎃: Halloween, 🎄: Christmas, 🦃: Thanksgiving, ❤️: Valentines Day, 🍀: St. Patricks Day
+type Holiday = "🎃" | "🎄" | "🦃" | "❤️" | "🍀";
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("❤️");
+
+    function nextAlpha(): void {
+        if (holiday === "🎄") {
+            setHoliday("🎃");
+        } else if (holiday === "🎃") {
+            setHoliday("🍀");
+        } else if (holiday === "🍀") {
+            setHoliday("🦃");
+        } else if (holiday === "🦃") {
+            setHoliday("❤️");
+        } else {
+            setHoliday("🎄");
+        }
+    }
+
+    function nextDate(): void {
+        if (holiday === "❤️") {
+            setHoliday("🍀");
+        } else if (holiday === "🍀") {
+            setHoliday("🎃");
+        } else if (holiday === "🎃") {
+            setHoliday("🦃");
+        } else if (holiday === "🦃") {
+            setHoliday("🎄");
+        } else {
+            setHoliday("❤️");
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <span>Holiday: {holiday}</span>
+            </div>
+            <div>
+                <Button onClick={nextAlpha}>Advance by Alphabet</Button>
+                <Button onClick={nextDate}>Advance by Year</Button>
+            </div>
+        </div>
+    );
 }
